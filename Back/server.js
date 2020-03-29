d44d49a0984c6c3c261a2da224efcb362b9989e4
saundersp <pierre.saundersgb@gmail.com>
2020-03-29 20:48:27 +0200

    Back: ajout fetch fichier NUTS au format kml

--- Back/server.js
+++ Back/server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 const server = require('http').Server(app);
 const mysql = require('mysql');
+const fetch = require('node-fetch');
+
+const { log, error } = console;
 
 const db_con = mysql.createConnection({
     host: 'localhost',
@@ -12,26 +15,28 @@ const db_con = mysql.createConnection({
 });
 
 db_con.connect(err => {
-    if (err) throw err;
-    console.log("Mysql connected !");
+    if (err)
+        error(err);
+    else
+        log("Mysql connected !");
 });
 
 app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", ["*"]);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-
     next();
 });
 
 // Lance le serveur avec express
 server.listen(port);
-console.log("Serveur lancé sur le port : " + port);
+log("Serveur lancé sur le port : " + port);
 
 app.get('/api/getNuts', (_, res) => {
     res.setHeader('Content-Type', 'text/json');
-    db_con.query("SELECT n.* FROM tls904_nuts as n", (err, result) => {
+    db_con.query("SELECT n.nuts as code, n.nuts_label as label FROM tls904_nuts as n WHERE n.nuts_level = 0", (err, result) => {
         if (err) {
+            error(err);
             res.statusCode = 500;
             res.send("{ error : '" + err + "'}");
         } else {
@@ -39,4 +44,16 @@ app.get('/api/getNuts', (_, res) => {
             res.send(JSON.stringify(result));
         }
     });
+});
+
+app.get('/api/getGeometry/:code', async (req, res) => {
+    try {
+        const b = await fetch(`http://nuts.geovocab.org/id/${req.params.code}_geometry.kml`);
+        const txt = await b.text();
+        res.send(txt);
+    } catch (err) {
+        error(err);
+        res.statusCode = 500;
+        res.send("{ error : '" + err + "'}");
+    }
 });
